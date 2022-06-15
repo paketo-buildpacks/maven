dd58e650fd7fe6a569c1427429db5f3dcd30fbb2
cz4rny <github@thezenofit.com>
2022-06-15 09:05:28 +0200

    Proper warnings lgging

--- maven/build.go
+++ maven/build.go
@@ -100,11 +100,11 @@ func (b Build) Build(context libcnb.BuildContext) (libcnb.BuildResult, error) {
 			return libcnb.BuildResult{}, fmt.Errorf("unable to stat %s\n%w", command, err)
 		} else {
 			if err := os.Chmod(command, 0755); err != nil {
-				fmt.Printf("WARNING: unable to chmod %s:\n%s", command, err)
+				b.Logger.Bodyf("WARNING: unable to chmod %s:\n%s", command, err)
 			}
 
 			if err = b.CleanMvnWrapper(command); err != nil {
-				fmt.Printf("WARNING: unable to clean mvnw file: %s\n%s", command, err)
+				b.Logger.Bodyf("WARNING: unable to clean mvnw file: %s\n%s", command, err)
 			}
 		}
 	}
